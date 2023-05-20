fc12c9d78348ad24fb5bb27166ed92bb759cab5a
Mohammad Hidayat Ullah Reza <91150098+Mohammad-Hidayat-Ullah-Reza@users.noreply.github.com>
2023-05-20 22:06:02 +0600

    social media link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,72 +12,72 @@ import {
 function Footer() {
   return (
     <footer className="bg-[#FEFEFE]">
-      <div className="container px-10 py-20 grid grid-cols-12 gap-7">
+      <div className="container grid grid-cols-12 gap-7 px-10 py-20">
         {/* logo and all rights */}
         <div className="col-span-12 md:col-span-6 lg:col-span-3">
           <a href="#" className="text-3xl font-semibold capitalize">
             Logo
           </a>
-          <p className="text-gray-500 text-sm mt-3 mb-5">
+          <p className="mb-5 mt-3 text-sm text-gray-500">
             Found in 2023, Pinpoint Proxies has focused itself on ultra-fast
             speeds.
           </p>
-          <p className="text-gray-400 text-sm">
+          <p className="text-sm text-gray-400">
             All rights reserved, <a href="#">Pinpoint Proxies</a>
           </p>
         </div>
 
         {/* Menu */}
-        <div className="col-span-6 md:col-span-3 lg:col-span-2 flex flex-col items-start gap-3 capitalize">
+        <div className="col-span-6 flex flex-col items-start gap-3 capitalize md:col-span-3 lg:col-span-2">
           <div className="text-xl font-semibold">Menu</div>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             our products
           </a>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             How we compare
           </a>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             pricing plans
           </a>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             FAQ
           </a>
         </div>
 
         {/* More */}
-        <div className="col-span-6 md:col-span-3 lg:col-span-2 flex flex-col items-start gap-3 capitalize">
+        <div className="col-span-6 flex flex-col items-start gap-3 capitalize md:col-span-3 lg:col-span-2">
           <div className="text-xl font-semibold">More</div>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             privacy policy
           </a>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             terms & conditions
           </a>
-          <a href="#" className="text-gray-500 text-sm hover:text-gray-700">
+          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
             refund policy
           </a>
         </div>
 
         {/* Contact */}
-        <div className="col-span-12 md:col-span-6 lg:col-span-3 flex flex-col items-start gap-3 capitalize">
+        <div className="col-span-12 flex flex-col items-start gap-3 capitalize md:col-span-6 lg:col-span-3">
           <div className="text-xl font-semibold">Contact</div>
           <a
             href="#"
-            className="text-gray-500 text-sm hover:text-gray-700 flex items-start gap-4"
+            className="flex items-start gap-4 text-sm text-gray-500 hover:text-gray-700"
           >
             <BsFillTelephoneFill className="text-xl" />
             (000) 000-0000
           </a>
           <a
             href="#"
-            className="text-gray-500 text-sm hover:text-gray-700 flex items-start gap-4"
+            className="flex items-start gap-4 text-sm text-gray-500 hover:text-gray-700"
           >
             <IoMdMail className="text-xl" />
             email@pinpointproxies.com
           </a>
           <a
             href="#"
-            className="text-gray-500 text-sm hover:text-gray-700 flex items-start gap-4"
+            className="flex items-start gap-4 text-sm text-gray-500 hover:text-gray-700"
           >
             <ImLocation className="text-xl" />
             2972 westheimer rd. santa ana, illinois 85486
@@ -85,30 +85,34 @@ function Footer() {
         </div>
 
         {/* Social Link */}
-        <div className="col-span-6 md:col-span-3 lg:col-span-2 flex flex-col items-start gap-3 capitalize">
+        <div className="col-span-6 flex flex-col items-start gap-3 capitalize md:col-span-3 lg:col-span-2">
           <div className="text-xl font-semibold normal-case">Social media</div>
-          <div className="flex items-center justify-between xl:pr-10 w-full">
+          <div className="flex w-full items-center justify-between xl:pr-10">
             <a
-              href="#"
-              className="text-gray-500 text-sm hover:text-gray-700 flex items-center gap-2"
+              href="https://www.fb.com"
+              target="_blank"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
             >
               <FaFacebookF className="text-xl" />
             </a>
             <a
-              href="#"
-              className="text-gray-500 text-sm hover:text-gray-700 flex items-center gap-2"
+              href="https://www.twitter.com"
+              target="_blank"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
             >
               <FaTwitter className="text-xl" />
             </a>
             <a
-              href="#"
-              className="text-gray-500 text-sm hover:text-gray-700 flex items-center gap-2"
+              href="https://www.linkedin.com"
+              target="_blank"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
             >
               <FaLinkedinIn className="text-xl" />
             </a>
             <a
-              href="#"
-              className="text-gray-500 text-sm hover:text-gray-700 flex items-center gap-2"
+              href="https://www.instagram.com"
+              target="_blank"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700"
             >
               <FaInstagram className="text-xl" />
             </a>
